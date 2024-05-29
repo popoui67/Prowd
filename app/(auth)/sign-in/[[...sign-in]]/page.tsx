25f810ebf20e4f071de94fb200d73ab2a7a630a7
popoui67 <140796567+popoui67@users.noreply.github.com>
2024-05-29 21:33:01 +0100

    Update page.tsx

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,8 +1,6 @@
-import { CreateUser, GetUser } from "@/lib/action/action.user";
+
 import { SignIn } from "@clerk/nextjs";
-import {  auth, currentUser } from "@clerk/nextjs/server";
-import { User } from "@prisma/client";
-import { redirect } from "next/navigation";
+
 export default async function Page() {
   
   return <SignIn path="/sign-in" />
