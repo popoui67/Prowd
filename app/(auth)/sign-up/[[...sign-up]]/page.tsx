9aab9e3fd6bbd232fd0a1b2342b3ea11cb33b86d
popoui67 <140796567+popoui67@users.noreply.github.com>
2024-05-29 21:33:45 +0100

    Update page.tsx

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,14 +1,10 @@
-import { CreateUser } from "@/lib/action/action.user";
+
 import { SignUp } from "@clerk/nextjs";
-import { auth, currentUser } from "@clerk/nextjs/server";
-import { User } from "@prisma/client";
 
 export  default async function Page() { 
-  const user = await currentUser() as User | any 
   return (
     <>
    <SignUp path="/sign-up"  />
-{ await CreateUser(user)}    
     </>
 
 );
